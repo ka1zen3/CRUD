770cf2eddada508b20cff170514854c7769e60db
ka1zen3 <bianca.cagurungan03@gmail.com>
2025-04-29 22:05:27 +0800

    .

--- main.php
+++ main.php
@@ -25,8 +25,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['edit']) && is_numeric
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Books Management</title>
+  <title>PLP Library</title>
   <link rel="stylesheet" href="main-style.css" />
+ <link rel="icon" type="image/png" href="plp-logo.png">
 </head>
 <body>
 
